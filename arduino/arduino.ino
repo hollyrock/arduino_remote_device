ceddb765593dfb190e24027f130a170b03a34d75
hollyrock <thoroiwa@gmail.com>
2015-10-19 17:00:55 +0900

    Modified debug comments

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -15,8 +15,7 @@ XBee xbee = XBee();
 
 uint8_t payload[] = {0, 0}; // allocate two bytes for to hold a 10-bit analog reading
 
-// AT Command for retreving XBee configurations on Arduino
-//uint8_t arryCmd[] = {'M', 'Y'};
+// AT Command for XBee on Arduino
 uint8_t ID[] = {'I','D'}; //Personal Area Network ID ...OK
 uint8_t MY[] = {'M','Y'}; //16-bit Address           ...OK
 //uint8_t DB[] = {'D','B'}; //Received Signal Strength ...NG
@@ -29,7 +28,6 @@ uint8_t FR[] = {'F','R'}; //Software Reset           ...OK
 
 Tx16Request tx = Tx16Request(0x1234, payload, sizeof(payload)); // Coordinator Address: 0x1234
 Rx16Response rx16 = Rx16Response();
-
 TxStatusResponse txStatus = TxStatusResponse();
 Rx16IoSampleResponse ioSample = Rx16IoSampleResponse(); 
 
@@ -48,19 +46,11 @@ int serial_speed = 19200;
 //uint8_t option = 0;
 uint8_t data = 0;
 
-void setup() {
-  pinMode(statusLed, OUTPUT);
-  pinMode(errorLed, OUTPUT);
-  
-  Serial.begin(serial_speed);     // Debug
-  altSerial.begin(serial_speed);  // Xbee
-  xbee.setSerial(altSerial);
-  
-
-  getParam();                 // Obtain self configration parameters
-}
-
-void flashLed(int pin, int times, int wait) { // Blink LED
+/******************************
+    Functions Parameters
+ *******************************/
+// ***** Blink LED
+void flashLed(int pin, int times, int wait) {
     for (int i = 0; i < times; i++) {
       digitalWrite(pin, HIGH);
       delay(wait);
@@ -69,7 +59,8 @@ void flashLed(int pin, int times, int wait) { // Blink LED
     }
 }
 
-void getParam(){ // Get current parameters on xbee
+// ***** Get current parameters on xbee
+void getParam(){
   atRequest.setCommand(ID);
   sendATCommand();
   atRequest.setCommand(MY);
@@ -78,7 +69,8 @@ void getParam(){ // Get current parameters on xbee
   sendATCommand();
 }
 
-int sendATCommand(){ // Use AT command to change xbee parameters
+// ***** Use AT command to change xbee parameters
+int sendATCommand(){
   int err = 0;
   xbee.send(atRequest); //send the command
       
@@ -86,26 +78,28 @@ int sendATCommand(){ // Use AT command to change xbee parameters
     if (xbee.getResponse().getApiId() == AT_COMMAND_RESPONSE) {
       xbee.getResponse().getAtCommandResponse(atResponse);
       if (atResponse.isOk()) {
-        Serial.print("Command [");
+        Serial.print("[DBG] Command [");
         Serial.write(atResponse.getCommand()[0]);
         Serial.write(atResponse.getCommand()[1]);
-        Serial.println("] was successful.");
+        Serial.println("] was successful.*****");
         if (atResponse.getValueLength() > 0) {
-          Serial.print("Return Value length: ");
+          Serial.print("[DBG] Return Value length: ");
           Serial.println(atResponse.getValueLength(), DEC);
           Serial.print("Value: ");
           for (int i=0; i<atResponse.getValueLength(); i++) {
             Serial.print(atResponse.getValue()[i], HEX);
           }
           Serial.println();
+          Serial.println("*************************");
         }
       }
       else {
-        Serial.print("Command [");
+        Serial.print("[DBG] Command [");
         Serial.write(atResponse.getCommand()[0]);
         Serial.write(atResponse.getCommand()[1]);
         Serial.print("] was failed with error code: ");
         Serial.println(atResponse.getStatus(), HEX);
+        Serial.println("*************************");
         err++;
       }
     }
@@ -113,8 +107,9 @@ int sendATCommand(){ // Use AT command to change xbee parameters
   return err;
 }
 
+// ***** Print frame content to serial monitor
 void debugPrint(Rx16IoSampleResponse sample){
-  Serial.println("DEBUG==========");
+  Serial.println("DEBUG========================");
   Serial.print("ID         :");
   Serial.println(sample.getRemoteAddress16(), HEX);
   Serial.print("Sample Size:");
@@ -129,6 +124,7 @@ void debugPrint(Rx16IoSampleResponse sample){
   Serial.println("==============================");
 }
 
+// ***** Control Command
 void commandDescriptor(uint8_t command){
 /*
  * Arduino is controlled by applying command:
@@ -147,7 +143,7 @@ void commandDescriptor(uint8_t command){
     case 0:
       break;
     case 2:
-      debugPrint(ioSample);       // Print frame content to serial monitor
+      debugPrint(ioSample);
       break;
     case 3:
       break;
@@ -169,7 +165,7 @@ void commandDescriptor(uint8_t command){
       Serial.println ("Bye now");
       break;
     case 131:
-      flashLed(statusLed, 3, 10); // Flash LED
+      flashLed(statusLed, 3, 10);
       break;
     case 193:
       break;
@@ -180,10 +176,27 @@ void commandDescriptor(uint8_t command){
       sendATCommand();
       break;
     default:
-      Serial.println ("Invalid command!");
+      Serial.println ("[DBG] Invalid command.*****");
     }  
 }
 
+/******************************
+    Setup
+ *******************************/
+void setup() {
+  pinMode(statusLed, OUTPUT);
+  pinMode(errorLed, OUTPUT);
+  
+  Serial.begin(serial_speed);    // Open serial port for debug console
+  altSerial.begin(serial_speed); // Open serial port for Xbee
+  xbee.setSerial(altSerial);
+  
+  getParam();                     // Obtain self configration parameters
+}
+
+/******************************
+    Loop
+ *******************************/
 void loop() {
   xbee.readPacket();
   if (xbee.getResponse().isAvailable()) {
@@ -197,26 +210,23 @@ void loop() {
         commandDescriptor(data);
       }
       
-      //Serial.print("I will send Temp. data to XBee having addr: ");
-      //Serial.println(ioSample.getRemoteAddress16(), HEX);
-      
-      // ===== End procedure ======
+      // ***** End procedure
       if (xbee.readPacket(5000)) { // after sending a tx request, expecting a status response (timeout:5000ms)
-        Serial.println("Wow! I've got a response!");
+        Serial.println("[DBG] There is a response.*****");
 
         if (xbee.getResponse().getApiId() == TX_STATUS_RESPONSE) { // should be a znet tx status
           xbee.getResponse().getZBTxStatusResponse(txStatus);
           
           if (txStatus.getStatus() == SUCCESS) { // get the delivery status, the fifth byte
             flashLed(statusLed, 5, 50);
-            Serial.println("Success! Congraturations!");
+            Serial.println("[DBG] Success.*****");
           } else {
-            Serial.println("a,oh we're in trouble..");
+            Serial.println("[DBG] Something wrong during end procedure.*****");
             flashLed(errorLed, 3, 500);
           }
         }     
       } else if (xbee.getResponse().isError()) {
-        Serial.println("Wow! I've got an Error response!");
+        Serial.println("[DBG] I've recieved error response.*****");
       } else {
          // local XBee did not provide a timely TX Status Response.  Radio is not configured properly or connected
          flashLed(errorLed, 2, 50);
